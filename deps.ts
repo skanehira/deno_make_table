adbc38c852337ce39e08b6c41ccfb40a938b8a04
skanehira <sho19921005@gmail.com>
2023-02-03 00:25:29 +0900

    refactor: replace to snapshot module

--- deps.ts
+++ deps.ts
@@ -1,3 +1,2 @@
 export { Table } from "https://deno.land/x/cliffy@v0.25.7/table/mod.ts";
-export { assertEquals } from "https://deno.land/std@0.175.0/testing/asserts.ts";
-export { join } from "https://deno.land/std@0.175.0/path/mod.ts";
+export { assertSnapshot } from "https://deno.land/std@0.175.0/testing/snapshot.ts";
--- table_test.ts
+++ table_test.ts
@@ -1,16 +1,15 @@
 import { makeTable } from "./table.ts";
-import { assertEquals, join } from "./deps.ts";
+import { assertSnapshot } from "./deps.ts";
 
 Deno.test({
   name: "make table",
-  fn: async () => {
+  fn: async (t) => {
     const input = `h1, h2
 col1, col2
 col3, col4
     `;
 
     const got = makeTable(input);
-    const want = await Deno.readTextFile(join("testdata", "expect_table.txt"));
-    assertEquals(got, want.trim());
+    await assertSnapshot(t, got);
   },
 });
