62d2595869dacec6ce9f0fd4268929b26d6c7c1b
skanehira <sho19921005@gmail.com>
2023-02-02 21:47:48 +0900

    chore: use testdata for compare result

--- table_test.ts
+++ table_test.ts
@@ -1,22 +1,16 @@
 import { makeTable } from "./table.ts";
-import { assertEquals } from "./deps.ts";
+import { assertEquals, join } from "./deps.ts";
 
 Deno.test({
   name: "make table",
-  fn: () => {
+  fn: async () => {
     const input = `h1, h2
 col1, col2
 col3, col4
     `;
 
     const got = makeTable(input);
-    const want = `┌──────┬──────┐
-│ h1   │ h2   │
-├──────┼──────┤
-│ col1 │ col2 │
-├──────┼──────┤
-│ col3 │ col4 │
-└──────┴──────┘`;
-    assertEquals(got, want);
+    const want = await Deno.readTextFile(join("testdata", "expect_table.txt"));
+    assertEquals(got, want.trim());
   },
 });
